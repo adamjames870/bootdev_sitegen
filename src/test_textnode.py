bd5044313c6813dc08d3cddbe09163190e2877c1
adamjames870 <adamjames870@yahoo.co.uk>
2025-04-12 16:37:58 +0800

    Creates textnode and tests

---
+++ src/test_textnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_default_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        self.assertIsNone(node.url)
+    
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+        
+    def test_not_eq_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is also a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+        
+    def test_not_eq_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+        
+    def test_not_eq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD, "url")
+        self.assertNotEqual(node, node2)
